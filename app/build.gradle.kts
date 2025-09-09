7bb8272e60f95cdf348fb6fd64ebff20124f8fe7
Je <jenniferjeo06@gmail.com>
2025-09-09 18:41:18 +0700

    Commit No. 4 – Standard activity and single top activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.lab_week_02_c"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.lab_week_02_c"
